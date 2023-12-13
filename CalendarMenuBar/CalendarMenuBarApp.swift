0295f4342c127f8d11589172ff8f1a79a74538ce
Kevin Ploß <65730791+pl0ss@users.noreply.github.com>
2023-12-13 23:04:12 +0100

    Initial Commit

---
+++ CalendarMenuBar/CalendarMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarMenuBarApp.swift
+//  CalendarMenuBar
+//
+//  Created by Kevin Ploß on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
